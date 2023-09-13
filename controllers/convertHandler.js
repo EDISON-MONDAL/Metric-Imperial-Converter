e4f38e3f9da8125defe0ba45ff36c2d90e8c7c94
Edison-Mondal <mondaledison@gmail.com>
2023-09-13 19:12:40 +0600

    ok

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -3,23 +3,59 @@ function ConvertHandler() {
   this.getNum = function(input) {
     let result;
     
+    if( input.match(/\d+\.\d+|\d+\/\d+|\d+/) ){
+
+      if(input.match(/\d+\/\d+\/\d+/)){ // more than one fraction 1/2/3
+        return NaN
+      }
+      else if(input.match(/\d+\.\d+\/\d+|\d+\/\d+/)){ // 1.2/3 | 2/3
+
+        if(input.match(/\d+\.\d+\/\d+/)){ // 1.2/3
+
+          const fraction = input.match(/\d+\.\d+\/\d+/)
+          const nominator = fraction[0].slice(0, fraction[0].indexOf("/"))
+          const dnominator = fraction[0].slice(fraction[0].indexOf("/")+1)
+          return nominator / dnominator
+
+        }else {
+          const fraction = input.match(/\d+\/\d+/)
+          const nominator = fraction[0].slice(0, fraction[0].indexOf("/"))
+          const dnominator = fraction[0].slice(fraction[0].indexOf("/")+1)
+          return nominator / dnominator
+        }
+      } else if(input.match(/\d+\.\d+|\d+/)){
+        result = Number(input.match(/\d+\.\d+|\d+\/\d+|\d+/))
+      }
+      
+    } else {
+      result = 1
+    }
+
+    
+    
     return result;
   };
   
   this.getUnit = function(input) {
     let result;
     
+    //result = input
+
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
+
+    //result = initUnit
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
+
+    //result = unit
     
     return result;
   };
@@ -29,12 +65,16 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
+
+    //result = {initNum, initUnit}
     
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
+
+    //result = {initNum, initUnit, returnNum, returnUnit}
     
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,49 @@
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
+
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+
+  // Define a route for the '/api/convert' endpoint
+  app.get('/api/convert', (req, res) => {
+    // You can access the query parameters using req.query
+    const input = req.query.input;
+    
+    // Implement your conversion logic using the ConvertHandler here
+    // For example:
+    const num = convertHandler.getNum(input);
+    const unit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(num, unit);
+    const returnUnit = convertHandler.getReturnUnit(unit);
+    const string = convertHandler.getString(num, unit, returnNum, returnUnit);
+
+
+
+    if(num == NaN){      
+      const error = new Error('invalid number');
+      error.stack = 'Error thrown from my code';
+
+      res.send(error.message)
+    }else {
+  
+
+      // Prepare the response JSON
+      const response = {
+        initNum: num,
+        initUnit: unit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: string,
+      };
+
+      // Send the JSON response
+      res.json(response);
+    }
+  
+    
+  });
+  
 
 };
