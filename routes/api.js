b3f22d4df4b8f4de92cd84ebcfd3a874ff50d41c
Edison-Mondal <mondaledison@gmail.com>
2023-09-15 10:01:03 +0600

    ok

--- routes/api.js
+++ routes/api.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
 
 
     
-    if(unit == 'invalid unit' && isNaN(num) ){
+    if(unit == 'invalid unit' && num == 'invalid number' ){
       console.log(num + ' ok num ' + unit + ' ok unit')
       res.send('invalid number and unit');
       //res.status(400).json({ error: 'invalid number and unit' });
